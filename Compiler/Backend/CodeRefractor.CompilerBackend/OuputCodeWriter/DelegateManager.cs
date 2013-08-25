b6c96637f7a251670d83c00fb8222863ca211ff8
Ciprian Khlud <ciprian.mustiata@yahoo.com>
2013-08-25 20:37:34 +0300

    Added delegate sample code. Useful for backing code of delegates using std::function

---
+++ Compiler/Backend/CodeRefractor.CompilerBackend/OuputCodeWriter/DelegateManager.cs
@@ -0,0 +1,62 @@
+﻿namespace CodeRefractor.CompilerBackend.OuputCodeWriter
+{
+    class DelegateManager
+    {
+        private const string ValidDelegatesCode = @"
+
+#include <vector>
+
+void (*callBack)(int, int);
+
+class CustomDelegateVoid_Int32_Int32 {
+	std::vector< std::function<void(int, int)> > functions;
+public:
+	void Invoke(int arg0, int arg1);
+	void Register(std::function<void(int, int)>  );
+};
+
+
+CustomDelegateVoid_Int32_Int32 callbackDelegate1;
+
+void RegisterCallbackDelegate1Void_Int32_Int32(){
+	callbackDelegate1.Register(callBack);
+}
+
+void callBackToDelegate(int arg0,int arg1){
+	callbackDelegate1.Invoke(arg0, arg1);
+}
+
+
+void methodToBeCalled(double customData, int arg0, int arg1){
+	
+}
+
+struct CallerClosure{
+
+	double customData;
+	CallerClosure(double data) {
+		customData = data;
+	}
+	void operator()(int arg0, int arg1) const {
+		methodToBeCalled(customData, arg0, arg1);
+	} 	
+};
+
+
+void RegisterMethodWithCustomData(){
+	callbackDelegate1.Register(std::function<void (int, int)> () = CallerClosure(2.3) ); 
+}
+
+void CustomDelegateVoid_Int32_Int32::Invoke(int arg0, int arg1){
+	for(auto it = functions.begin(); it!= functions.end(); ++it){
+		(*it)(arg0, arg1);
+	}
+}
+
+void CustomDelegateVoid_Int32_Int32::Register(std::function<void(int, int)> stdFn ){
+	//std::function<void(int, int)> stdFn(fn);
+	functions.push_back(stdFn);
+}
+";
+    }
+}
