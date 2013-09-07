0cbf07016415f325d893b159cb95bd84ae09df2f
Ciprian Khlud <ciprian.mustiata@yahoo.com>
2013-09-07 21:33:14 +0300

    Added code to be fixed

--- VmTests/Projects/SimpleAdditions/NBody.cs
+++ VmTests/Projects/SimpleAdditions/NBody.cs
@@ -6,19 +6,11 @@ namespace Figures
 {
     class Program
     {
-        public static int OsNewsBench32(int maxValue)
-        {
-            var result = 1;
-            for(var i = 1; i< maxValue; i+=4)
-            {
-                result = -i + (i + 1)*(i + 2)/(i + 3);
-            }
-            return result;
-        }
         public static void Main()
         {
-            var compute = OsNewsBench32(1000000000);
-            Console.WriteLine(compute);
+            var charData = new[] {'H','e','l','l','o',' ','w','o','r','l','d'};
+            var s = new string(charData);
+            Console.WriteLine(s);
         }
     }
 }
