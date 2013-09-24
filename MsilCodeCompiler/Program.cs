692bfac8aaad40c41e64f515511509bcd48151e3
Ciprian Khlud <ciprian.mustiata@yahoo.com>
2013-09-24 20:48:28 +0300

    Setting NBody source code with hardcoded 5 000 000 iterations.

--- MsilCodeCompiler/Program.cs
+++ MsilCodeCompiler/Program.cs
@@ -64,7 +64,7 @@ namespace CodeRefractor.Compiler
             //MetaLinker.ScanAssembly(typeof(int));
             //MetaLinker.ScanAssembly(typeof(Console));
             NativeCompilationUtils.SetCompilerOptions("gcc");
-            CommandLineParse.OptimizerLevel =0;
+            CommandLineParse.OptimizerLevel =2;
             CallCompiler("", "");
             //var standardOutput = applicationNativeExe.ExecuteCommand();
             //Console.WriteLine(standardOutput);
