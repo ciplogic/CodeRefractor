35901be38a05158cbf145c64a259ada16fbaab9f
Ciprian Khlud <ciprian.mustiata@yahoo.com>
2013-11-06 18:57:51 +0200

    Added version

--- Common/CodeRefractor.RuntimeBase/Config/CodeRefractorVersions.cs
+++ Common/CodeRefractor.RuntimeBase/Config/CodeRefractorVersions.cs
@@ -2,6 +2,6 @@ namespace CodeRefractor.RuntimeBase.Config
 {
     public static class CodeRefractorVersions
     {
-        public const string Version = "0.0.1";
+        public const string Version = "0.0.1_pre";
     }
 }
