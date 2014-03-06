5d760648f6b0b0f01a99899f86ef3e8827cb4240
CiprianKhlud <ciprian.mustiata@gmail.com>
2014-03-06 21:21:31 +0300

    Added a simple application

---
+++ Views/CR.Code.Editor/Views/NotificationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace CR.Code.Editor.Views
+{
+    public class NotificationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the Changed method to raise the event 
+        void Changed(string name)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void Changed<T>(Expression<Func<T>> propertyName)
+        {
+            var body = propertyName.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("'propertyExpression' should be a member expression");
+
+            Changed(body.Member.Name);
+        }
+    }
+}
