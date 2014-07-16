d4718b90440a92f31a814eeb200d8543766717a3
Ciprian Khlud <ciprian.mustiata@gmail.com>
2014-07-16 22:43:02 +0300

    Define type sorter that will work without hanging

---
+++ Common/CodeRefractor.RuntimeBase/ClosureCompute/TypeSort/ClosureTypeSorter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeRefractor.ClosureCompute.TypeSort
+{
+    class ClosureTypeSorter
+    {
+        private readonly HashSet<Type> _types;
+
+        public ClosureTypeSorter(Type[] types)
+        {
+            _types = new HashSet<Type>();
+            foreach (var type in types)
+            {
+                _types.Add(type);
+            }
+        }
+
+        public List<Type> DoSort()
+        {
+            var result = new List<Type>(_types);
+            result.Sort(new TypeComparer());
+            return result;
+        }
+    }
+}
