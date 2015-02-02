504cd4bca5b287a215f3a6ae7001af90a66ff3d6
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-02-02 16:45:27 +0100

    Fix #11. Use a sample without try/catch that compiles fine.

--- VmTests/Projects/SimpleAdditions/App.cs
+++ VmTests/Projects/SimpleAdditions/App.cs
@@ -1,57 +1,19 @@
-//http://www.dotnetperls.com/interface
-
+//Taken From Microsoft Documentation on Virtual Methods
 using System;
-using IDisp;
+using System.Runtime.InteropServices;
 
-namespace IDisp
+/**
+ * Display a message box imported from an external source.
+ */
+public class Test
 {
-    internal class PersistentData : IDisposable
+    // this must be MessageBoxW, since in CR characters are char_w, and not char.
+    [DllImport("User32.dll")]
+    public static extern int MessageBoxW(int handle, String message, String title, uint type);
+
+    [STAThread]
+    public static void Main()
     {
-        private int enabled = 1;
-
-        public PersistentData()
-        {
-            Console.WriteLine("Enabled");
-        }
-
-        public void Dispose()
-        {
-            enabled = 0;
-            Console.WriteLine("Not enabled");
-
-        }
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        try
-        {
-            Console.WriteLine("content");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Inside catch");
-        }
-        finally
-        {
-
-            Console.WriteLine("finally");
-        }
-        /*
-        IValue value1 = new Image();
-        IValue value2 = new Article();
-
-        value1.Count++; // Access int property on interface
-        value2.Count++; // Increment
-
-        value1.Name = "Mona Lisa"; // Use setter on interface
-        value2.Name = "Resignation"; // Set
-
-        Console.WriteLine(value1.Name); // Use getter on interface
-        Console.WriteLine(value2.Name); // Get
-         */
+        MessageBoxW(0, "Message", "Title", 0);
     }
 }
--- bin/output.cpp
+++ bin/output.cpp
@@ -2,74 +2,45 @@
 #include <functional>
 struct System_Object;
 struct CodeRefactor_OpenRuntime_CrString;
-struct _Program;
-struct IDisp_PersistentData;
-struct CodeRefactor_OpenRuntime_CrConsole;
+struct _Test;
 struct System_Object {
     int _typeId;
 };
 
 struct System_String : public System_Object {
     System_String() {
-        _typeId = 6;
+        _typeId = 5;
     }
 
     std::shared_ptr< Array < System_Char > > Text;
 };
 
-struct _Program : public System_Object {
+struct _Test : public System_Object {
 };
 
-struct IDisp_PersistentData : public System_Object {
-    System_Int32 enabled;
-};
-
-struct System_Console : public System_Object {
-};
-
-System_Void _Program_Main();
-System_Void IDisp_PersistentData_ctor(IDisp_PersistentData* _this);
-System_Void System_Console_WriteLine(std::shared_ptr<System_String> value);
-System_Void IDisp_PersistentData_Dispose(IDisp_PersistentData* _this);
+System_Void _Test_Main();
+System_Int32 _Test_MessageBoxW(System_Int32 handle, System_Char* message, System_Char* title, System_UInt32 type);
 
 #include "runtime_base.hpp"
 
 // --- Begin definition of virtual implementingMethod tables ---
 System_Void setupTypeTable();
 
-#include "stdio.h"
-System_Void System_Console_WriteLine(std::shared_ptr<System_String> value) {
-    printf("%ls\n", value.get()->Text->Items);
-}
 ///--- PInvoke code ---
+typedef System_Int32 ( *dll_method_1_type)(System_Int32 handle, System_Char* message, System_Char* title, System_UInt32 type);
+dll_method_1_type dll_method_1;
+
+System_Int32 _Test_MessageBoxW(System_Int32 handle, std::shared_ptr<System_String> message, std::shared_ptr<System_String> title, System_UInt32 type) {
+    System_Char* _message = message->Text->Items;
+    System_Char* _title = title->Text->Items;
+
+    return dll_method_1(handle, _message, _title, type);
+}
 ///---Begin closure code ---
-System_Void _Program_Main() {
-    IDisp_PersistentData * local_0;
-    System_Boolean local_1;
-    std::shared_ptr<IDisp_PersistentData> vreg_1;
+System_Void _Test_Main() {
+    System_Int32 vreg_1;
     
-    vreg_1 = std::make_shared<IDisp_PersistentData >();vreg_1->_typeId = 3;
-    IDisp_PersistentData_ctor(vreg_1);
-    local_0 = (vreg_1).get();
-    System_Console_WriteLine(_str(0));
-    local_1 = (vreg_1 == nullptr)?1:0;
-    if(local_1) goto label_25;
-    IDisp_PersistentData_Dispose(local_0);
-    label_25:
-
-    return;
-}
-System_Void IDisp_PersistentData_ctor(IDisp_PersistentData* _this) {
-    
-    _this->enabled = 1;
-    System_Console_WriteLine(_str(1));
-
-    return;
-}
-System_Void IDisp_PersistentData_Dispose(IDisp_PersistentData* _this) {
-    
-    _this->enabled = 0;
-    System_Console_WriteLine(_str(2));
+    vreg_1 = _Test_MessageBoxW(0, _str(0), _str(1), 0);
 
     return;
 }
@@ -78,12 +49,18 @@ System_Void initializeRuntime();
 int main(int argc, char**argv) {
     auto argsAsList = System_getArgumentsAsList(argc, argv);
     initializeRuntime();
-    _Program_Main();
+    _Test_Main();
 
     return 0;
 }
 
 System_Void mapLibs() {
+    //---------------------------------------------------------
+    // User32.dll methods
+    //---------------------------------------------------------
+    auto lib_0 = LoadNativeLibrary(L"User32.dll");
+
+    dll_method_1 = (dll_method_1_type)LoadNativeMethod(lib_0, "MessageBoxW");
 }
 
 
@@ -91,13 +68,11 @@ System_Void RuntimeHelpersBuildConstantTable() {
 }
 
 System_Void buildStringTable() {
-    _AddJumpAndLength(0, 16);
-    _AddJumpAndLength(17, 7);
-    _AddJumpAndLength(25, 11);
+    _AddJumpAndLength(0, 7);
+    _AddJumpAndLength(8, 5);
 } // buildStringTable
 
-const System_Char _stringTable[37] = {
-    117, 115, 105, 110, 103, 32, 112, 101, 114, 115, 105, 115, 116, 101, 110, 116, 0 /* "using persistent" */, 
-    69, 110, 97, 98, 108, 101, 100, 0 /* "Enabled" */, 
-    78, 111, 116, 32, 101, 110, 97, 98, 108, 101, 100, 0 /* "Not enabled" */
+const System_Char _stringTable[14] = {
+    77, 101, 115, 115, 97, 103, 101, 0 /* "Message" */, 
+    84, 105, 116, 108, 101, 0 /* "Title" */
 }; // _stringTable
